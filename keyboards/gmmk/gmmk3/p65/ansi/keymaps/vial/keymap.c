5b99fae422179a2e518e9506d585a14dce6169ec
theadept024 <theadept@gmail.com>
2026-04-16 22:18:59 -0600

    Fix LAYOUT argument count to 64 keys for p65 ansi keymaps

--- keyboards/gmmk/gmmk3/p65/ansi/keymaps/vial/keymap.c
+++ keyboards/gmmk/gmmk3/p65/ansi/keymaps/vial/keymap.c
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             MO(_FN),  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT, KC_RALT,                   KC_SPC,                             MO(_FN),  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_FN] = LAYOUT(
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RM_TOGG, RM_NEXT, RM_HUEU, RM_SATU, RM_VALU, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP,
-        _______, _______, _______,                            _______,                            TO(_LIGHT), KC_MUTE, KC_HOME, KC_PGDN, KC_END
+        _______, _______, _______, _______,                   _______,                            TO(_LIGHT), KC_MUTE, KC_HOME, KC_PGDN, KC_END
     ),
 
     [_GAME] = LAYOUT(
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, _______, _______, _______, _______, _______, _______, _______,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______, _______, _______, _______, _______, _______,          _______,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______, _______, _______, _______, _______, _______,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             TO(_BASE), _______, _______, _______, _______
+        KC_LCTL, KC_LGUI, KC_LALT, _______,                   KC_SPC,                             TO(_BASE), _______, _______, _______, _______
     ),
 
     [_LIGHT] = LAYOUT(
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, AC_TOG,  AC_HUD,  AC_HUI,  AC_VAD,  AC_VAI,  RM_TOGG, RM_PREV, RM_HUED, RM_HUEU, RM_VALD, RM_VALU, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______,                            _______,                            TO(_BASE), _______, _______, _______, _______
+        _______, _______, _______, _______,                   _______,                            TO(_BASE), _______, _______, _______, _______
     ),
 };
 
